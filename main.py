b45b925c9fbe0bc061814fd91c73d3787473b773
N-Patarov <nikolayp1707@gmail.com>
2021-02-20 20:04:34 +0200

    Added crypto

--- main.py
+++ main.py
@@ -6,21 +6,31 @@ import time
 def main():
     print(" WELLCOME TO PRICEWATCH ")
     print(" by NPatarov ")
+    lst=["btc","eth","xrp","dot","doge","xmr"]
+    cryptos=str(lst)
     run=True
     while run:
-        ask=input("CHOOSE btc, eth, xrp or q to quit: ").lower()
+        print("SUPORTED CRYPTOCURRENCIES"+cryptos)
+        ask=input("CHOOSE crypto, all to display all or q to quit: ").lower()
         if ask == "xrp":
             xrp()
-        elif ask == "xrpt":
-            xrpt()
+        elif ask == "xmr":
+            xmr()
+        elif ask == "doge":
+            doge()
         elif ask == "btc":
             btc()
-        elif ask == "btct":
-            btct()
         elif ask == "eth":
             eth()
-        elif ask == "etht":
-            etht()
+        elif ask == "dot":
+            dot()
+        elif ask == "all":
+            btc()
+            eth()
+            xrp()
+            doge()
+            xmr()
+            dot()
         elif ask == "q":
             run=False
         
@@ -44,31 +54,23 @@ def eth():
     price =  soup.find(class_='priceValue___11gHJ')
     print("ETH: "+price.text)
 
+def dot():
+    link = requests.get("https://coinmarketcap.com/currencies/polkadot/")
+    soup = BeautifulSoup(link.content,'html.parser')
+    price =  soup.find(class_='priceValue___11gHJ')
+    print("DOT: "+price.text)
 
-def xrpt():
-    while True:
-        link = requests.get("https://coinmarketcap.com/currencies/xrp/")
-        soup = BeautifulSoup(link.content,'html.parser')
-        price =  soup.find(class_='priceValue___11gHJ')
-        print("XRP: "+price.text)
-        time.sleep(30)
+def xmr():
+    link = requests.get("https://coinmarketcap.com/currencies/monero/")
+    soup = BeautifulSoup(link.content,'html.parser')
+    price =  soup.find(class_='priceValue___11gHJ')
+    print("XMR: "+price.text)
 
-def btct():
-    while True:
-        link = requests.get("https://coinmarketcap.com/currencies/bitcoin/")
-        soup = BeautifulSoup(link.content,'html.parser')
-        price =  soup.find(class_='priceValue___11gHJ')
-        print("BTC: "+price.text)
-        time.sleep(30)
-        
-def etht():
-    while True:
-        link = requests.get("https://coinmarketcap.com/currencies/ethereum/")
-        soup = BeautifulSoup(link.content,'html.parser')
-        price =  soup.find(class_='priceValue___11gHJ')
-        print("ETH: "+price.text)
-        time.sleep(30)
-
+def doge():
+    link = requests.get("https://coinmarketcap.com/currencies/dogecoin/")
+    soup = BeautifulSoup(link.content,'html.parser')
+    price =  soup.find(class_='priceValue___11gHJ')
+    print("DOGE: "+price.text)
 
 if __name__ == '__main__':
     main()
